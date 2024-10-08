86b0a6cb2566a885543b4c0c932c890e8defe70b
Mark <metalnight24@hotmail.com>
2024-10-08 12:22:57 -0400

    Routing Metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,24 @@
+import { Metadata } from "next";
+
+export const generateMetadata = async ({
+	params,
+}: Props): Promise<Metadata> => {
+	const title = await new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(`iPhone ${params.productId}`), 100;
+		});
+	});
+	return {
+		title: `Product ${title}`,
+	};
+};
+
+type Props = {
+	params: {
+		productId: string;
+	};
+};
+
 export default function ProductDetails({
 	params,
 }: {
