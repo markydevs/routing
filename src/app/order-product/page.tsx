71a42fecdf5129b3c66e3b9552997a8552b1deb1
Mark <metalnight24@hotmail.com>
2024-10-09 11:14:15 -0400

    Navigating Programmatically

---
+++ src/app/order-product/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+
+export default function OrderProduct() {
+	const router = useRouter();
+	const handleClick = () => {
+		console.log("Placing your order");
+		router.replace("/");
+	};
+	return (
+		<>
+			<h1>Order Product</h1>
+			<button onClick={handleClick}>Place order</button>
+		</>
+	);
+}
