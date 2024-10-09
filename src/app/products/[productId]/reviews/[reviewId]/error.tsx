8c74938a917070b3d36b78a0f39f945c10c26836
Mark <metalnight24@hotmail.com>
2024-10-09 12:21:58 -0400

    Recovering from Errors

--- src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -2,6 +2,16 @@
 
 import React from "react";
 
-export default function ErrorBoundary({ error }: { error: Error }) {
-	return <div>{error.message}</div>;
+export default function ErrorBoundary({
+	error,
+	reset,
+}: {
+	error: Error;
+	reset: () => void;
+}) {
+	return (
+		<div>
+			{error.message} <button onClick={reset}>Try again</button>
+		</div>
+	);
 }
