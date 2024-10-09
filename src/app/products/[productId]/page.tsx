01ad81ca278b5949e9c74bf5ca16e1f128c7cf88
Mark <metalnight24@hotmail.com>
2024-10-09 12:18:22 -0400

    Error Handling

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,11 +1,14 @@
+/* eslint-disable no-unused-expressions */
+
 import { Metadata } from "next";
 
 export const generateMetadata = async ({
 	params,
 }: Props): Promise<Metadata> => {
+	const { productId } = params;
 	const title = await new Promise((resolve) => {
 		setTimeout(() => {
-			resolve(`iPhone ${params.productId}`), 100;
+			return resolve(productId), 100;
 		});
 	});
 	return {
